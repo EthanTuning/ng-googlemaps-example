f4db93db3f0df35df0702fc90944c0aec23bca58
tuningfreelance <tuningfreelance@gmail.com>
2019-06-26 14:00:38 -0700

    got map working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
 @Component({
+
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+
 })
+
 export class AppComponent {
+  
   title = 'ng-googlemaps-example';
+  initialLat: number = 47.6587803;
+  initialLong: number = -117.4260466;
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AgmCoreModule } from '@agm/core';
 
 import { AppComponent } from './app.component';
+import apiConfig from './keys.json';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey: apiConfig.apiKey
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
